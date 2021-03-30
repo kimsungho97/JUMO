1c971c515865c956db4652e12921c6d26d39790d
kimsungho <kimsungho97@gmail.com>
2021-03-30 17:49:22 +0900

    프로젝트 초기 설정 완료

---
+++ src/test/java/JUMO/project/JumoApplicationTests.java
@@ -0,0 +1,13 @@
+package JUMO.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
