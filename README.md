9e98bd5b3784097d247d429c1c078b7d0d9c1653
kidjung <kjpark4321@naver.como>
2021-10-09 18:17:56 +0900

    add ERD on README and close #5

---
+++ README.md
@@ -0,0 +1,6 @@
+## 데이터 베이스 ERD
+
+---
+
+![JUMO ERD](https://user-images.githubusercontent.com/58928739/136652115-bf058de5-4e9e-4cb9-bbef-4c700083297d.png)
+
---
+++ src/test/java/JUMO/project/service/PropertyTest.java
@@ -0,0 +1,28 @@
+package JUMO.project.service;
+
+import JUMO.project.Service.PredictService;
+import JUMO.project.SpringConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+public class PropertyTest {
+
+    @Value("#{system['predict.URL']}")
+    private String myValue;
+
+//    private final PredictService predictService;
+
+    ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfig.class);
+    @Test
+    void getPropertyTest(){
+//        System.out.println(myValue);
+//        Assertions.assertThat(myValue).isEqualTo("localhost:5000");
+        System.out.println("add = " + myValue);
+    }
+}
