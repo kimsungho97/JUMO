c1ff454f0e5348ab596891ef8fe8caa9b341bccb
kidjung <kjpark4321@naver.como>
2021-10-18 19:07:31 +0900

    feat:  예외 클래스 생성
    - 구매 자금 부족 예외 클래스
    - 지갑 없음 예외 클래스
    - 판매시 보유 주식 부족 클래스

---
+++ src/main/java/JUMO/project/exception/NotEnoughMoneyException.java
@@ -0,0 +1,7 @@
+package JUMO.project.exception;
+
+public class NotEnoughMoneyException extends RuntimeException{
+    public NotEnoughMoneyException(String message) {
+        super(message);
+    }
+}
